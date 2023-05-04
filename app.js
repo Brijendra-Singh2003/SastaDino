3302f4b01848637f7ea7964e3b4d9f8f515aa634
Brijendra Singh <113870500+Brijendra-Singh2003@users.noreply.github.com>
2023-05-04 08:44:37 +0530

    new gameover animation

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const dino = document.getElementById('dino')
-let hit = true;
+let hit = false
 const score = document.getElementById('score')
+const hscore = document.getElementById('hScore')
+let highScore = ()=>{
+    if(localStorage.getItem('highScore') == null)
+    return 0
+    else
+    return localStorage.getItem('highScore')
+}
+window.onload = ()=>{
+    hscore.innerHTML = 'High Score: ' + highScore()
+}
 let count = 0;
 let t;
 
@@ -20,13 +30,13 @@ function jump() {
 }
 
 function start(interval) {
-    if(!hit) {
-        setTimeout(() => {
-            interval = (Math.floor(Math.random()*15 + 5) * 100);
-            start(interval)
-            generateBarriers()
-        }, interval)
-    }
+
+    setTimeout(() => {
+        if(hit) return;
+        interval = (Math.floor(Math.random()*15 + 5) * 100);
+        start(interval)
+        generateBarriers()
+    }, interval)
 }
 
 function generateBarriers() {
@@ -36,51 +46,62 @@ function generateBarriers() {
 
     setTimeout(() => {
         d2.style.right = '100%'
-    }, 20);
+    }, 2);
 
-    setTimeout(() => {
-
-        if(dino.offsetTop > 250) {
+    const t = setInterval(()=> {
+        if(d2.offsetLeft < 150 && d2.offsetLeft > 50 && dino.offsetTop > 250) {
             crash()
+            clearInterval(t)
             return
         }
-        
-        setTimeout(() => {
-            if(dino.offsetTop > 250) {
-                crash()
-                return
-            }
-        }, 140);
-
-    }, 1730);
+    }, 1)
 
     t = setTimeout(() => {
-        count++;
-        score.innerHTML = `SCORE: ${count}`
-        d2.remove()
-    }, 2020);
+        clearInterval(t)
+        if(!hit) {
+            count++;
+            score.innerHTML = `SCORE: ${count}`
+            d2.remove()
+        }
+    }, 2002);
 }
 
 window.onkeydown = (e) => {
-    if(e.keyCode == 32) {
-        if(!hit) {jump()}
-        else {
-            dino.removeAttribute('style')
-            dino.classList.remove('down')
-            hit = false;
-            count = 0;
-            score.innerHTML = `SCORE: ${count}`
-            document.getElementsByClassName('crash')[0].style.scale = 0;
-            start(2000)
-        }
+    if(e.keyCode == 32 && !hit) {jump()}
+    else if(e.keyCode == 13) {
+        if(hit)
+        location.href = location.href
+        start(2000)
+        document.getElementsByClassName('crash')[0].style.scale = 0;
     }
 }
 
+window.onclick = (e) => {
+    if(!hit) {jump()}
+}
+
 function crash() {
-    count--;
     hit = true;
-    dino.style.rotate = '90deg'
-    dino.style.bottom = '75px'
+    // dino.style.bottom = '75px'
+    dino.classList.remove('down')
+    dino.classList.add('up')
+
+    setTimeout(() => {
+        dino.classList.add('down')
+        dino.style.rotate = '90deg'
+        dino.classList.remove('up')
+    }, 250);
+    
+    dino.style.rotate = '75deg'
     document.getElementsByClassName('crash')[0].style.scale = 1;
     document.getElementsByClassName('crash')[0].innerHTML = 'GAME OVER';
+    if(highScore() < count) {
+        localStorage.setItem('highScore', count)
+    }
+    count--;
+    let arr = document.getElementsByClassName('obs')
+    for(item of arr) {
+
+        item.style.left = item.offsetLeft + 'px'
+    }
 }
